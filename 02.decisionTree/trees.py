e7521970598b99abfe632fb98bee77ff71897b39
Lee <jun5@live.missouristate.edu>
2018-02-16 20:24:24 -0600

    commenting and ordering

--- 02.decisionTree/trees.py
+++ 02.decisionTree/trees.py
@@ -1,6 +1,5 @@
 
 from math import log
-import operator
 from collections import Counter
 import pandas as pd
 
@@ -24,77 +23,81 @@ training_data2 = [
 ]
 
 
-def createDataSet(data):
+def createDataTableCsv(data):
+    '''
+    Takes data in form of pandas dataFrame that was extracted from .csv file. Note that the labels are manually
+    typed in. As long as the data from .csv can return list of dataTable and list of dataLabels, the algorithm should work.
+    '''
+
+    dataLabels= ['sepal length', 'sepal width', 'petal length', 'petal width']
+    dataTable = training_data1.values.tolist() # Use .values to get a numpy.array and then .tolist() to get a list.
+    dataTable.append(dataLabels)
+    return dataTable, dataLabels
+
+def createDataTable(data):
+    '''
+    Takes data in form of tuple that contains dictionary of attribute:value format and the classification.
+    returns tuple of dataTable list and labels list so that it can be used for the algorithm.
+    '''
 
     dataTable = []
-
     for tup in data:
         dataRow = list(tup[0].values())
         dataRow.append(tup[1])
         dataTable.append(dataRow)
         labels = list(tup[0].keys())
-
     return dataTable, labels
 
-#similar method but for extracting from the dataFrame
-def createDataSetCsv(data):
-    dataLabels= ['sepal length', 'sepal width', 'petal length', 'petal width']
-
-    dataTable = training_data1.values.tolist() #Use .values to get a numpy.array and then .tolist() to get a list.
-    #dataTable.append(dataLabels)
-    #print(dataTable[0])
-
-    return dataTable, dataLabels
-
-def entropy(dataSet):
-    labelCounts = {}
-    for featVec in dataSet:  # the the number of unique elements and their occurance
-        currentLabel = featVec[-1]
-        if currentLabel not in labelCounts.keys(): labelCounts[currentLabel] = 0
-        labelCounts[currentLabel] += 1
-    shannonEnt = 0.0
-    for key in labelCounts:
-        prob = float(labelCounts[key]) /  len(dataSet)
-        shannonEnt -= prob * log(prob, 2)  # log base 2
-    return shannonEnt
-
-
-def splitDataSet(dataSet, axis, value):
-    retDataSet = []
-    for featVec in dataSet:
-        if featVec[axis] == value:
-            reducedFeatVec = featVec[:axis]  # chop out axis used for splitting
-            reducedFeatVec.extend(featVec[axis + 1:])
-            retDataSet.append(reducedFeatVec)
-    return retDataSet
-
-
-def NodeSelection(dataTable):
+def createSubtable(dataTable, index, value):
     '''
-    Calculates the entropy of the whole system first, then iterates o
+    returns sub-dataTable based on the given dataTable, index of the attribute and its value.
     '''
-    entropy_for_system = entropy(dataTable)
-    highest_info_gain = 0.0;
-    selected_attribute = 0
-    attributesCount = len(dataTable[0]) - 1
 
-    for i in range(attributesCount):
-        attributes_all = [row[i] for row in dataTable]
-        attributes = set(attributes_all)
-        finalEntropy = 0.0
-        for att in attributes:
-            subTable = splitDataSet(dataTable, i, att)
-            prob = len(subTable) / float(len(dataTable))
-            finalEntropy += prob * entropy(subTable)
+    subDataTable = []
+    for row in dataTable:
+        if row[index] == value:
+            chopped_row = row[:index]  # setting the values that are BEFORE the index
+            chopped_row.extend(row[index + 1:])  # setting the value that are AFTER the index
+            subDataTable.append(chopped_row) # adding both of them, eventually returning reduced dataTable.
+    return subDataTable
 
+def createTree(dataTable, labels):
+    '''
+    Generates tree based on the dataTable given. dataTable shrinks over time which allows recursive algorithm.
+    '''
 
-        information_gain = entropy_for_system - finalEntropy  # calculate the info gain; ie reduction in entropy
+    decision = [row[-1] for row in dataTable]
+    if decision.count(decision[0]) == len(decision):
+        return decision[0]              # return when all of the decision in the dataTable is same
+    if len(dataTable[0]) == 1:
+        return majority_voting(decision)    # return if there is only one remaining feature in dataTable
+    root_attribute = NodeSelection(dataTable)
+    root_attributeLabel = labels[root_attribute]
+    idTree = {root_attributeLabel: {}}
+    del (labels[root_attribute])  # reducing the dataTable so the recursion will work
+    attributeValuesAll = [row[root_attribute] for row in dataTable]
+    attribute_values = set(attributeValuesAll)  # finding the unique values
+    for value in attribute_values:
+        sub_labels = labels[:]
+        idTree[root_attributeLabel][value] = createTree(createSubtable(dataTable, root_attribute, value), sub_labels)
+    return idTree
 
-        if (information_gain > highest_info_gain):  # compare this to the best gain so far
-            highest_info_gain = information_gain  # if better than current best, set to best
-            selected_attribute = i
-    return selected_attribute  # returns an integer
+def entropy(dataTable):
+    '''
+    Calculates entropy based on a given table. Iterates through dataTable gets the number for each attribute.
+    Then goes through calculation and eventually returns the entropy.
+    '''
 
+    decisionCounts = {}
+    for row in dataTable:
+        currentDecision = row[-1]
+        if currentDecision not in decisionCounts.keys(): decisionCounts[currentDecision] = 0
+        decisionCounts[currentDecision] += 1  # Common way of counting things in a form of dictionary
+    entropy = 0.0
+    for decision in decisionCounts:
+        probability = float(decisionCounts[decision]) / len(dataTable)
+        entropy -= probability * log(probability,2)
+    return entropy
 
 def majority_voting(decision):
     '''
@@ -103,32 +106,36 @@ def majority_voting(decision):
 
     return Counter(decision).most_common()[0][1] > (len(decision) // 2)
 
+def NodeSelection(dataTable):
+    '''
+    Calculates the entropy of the whole system first, then calcaultes the entropy of the certain attribute
+    which then is used to calculate the information gain. Index of attribute with highest
+    information gain is returned.
+    '''
 
-def createTree(dataTable, labels):
-    decision = [row[-1] for row in dataTable]  #[y, n n n n n n n]
-    if decision.count(decision[0]) == len(decision):
-        return decision[0]              # return when all of the decision in the dataTable is same
-    if len(dataTable[0]) == 1:
-        return majority_voting(decision)    # return if there is only one remaining feature in dataTable
+    entropy_for_system = entropy(dataTable)
+    highest_info_gain = 0.0
+    selected_attribute = 0
 
-    #### Changed up to this part.
-
-    bestFeat = NodeSelection(dataTable)
-    bestFeatLabel = labels[bestFeat]
-
-    myTree = {bestFeatLabel: {}}
-    del (labels[bestFeat])
-    featValues = [example[bestFeat] for example in dataTable]
-    uniqueVals = set(featValues)
-    for value in uniqueVals:
-        subLabels = labels[:]  # copy all of labels, so trees don't mess up existing labels
-        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataTable, bestFeat, value), subLabels)
-    return myTree
-
-
-
+    for i in range(len(dataTable[0]) - 1):
+        attributes_all = [row[i] for row in dataTable] # adding all attributes
+        attributes = set(attributes_all)  # store the unique attributes
+        finalEntropy = 0.0
+        for att in attributes:
+            subTable = createSubtable(dataTable, i, att)
+            probability = len(subTable) / float(len(dataTable))
+            finalEntropy += probability * entropy(subTable)
+        information_gain = entropy_for_system - finalEntropy  # calculate the info gain; ie reduction in entropy
+        if information_gain > highest_info_gain:  # compare this to the best gain so far
+            highest_info_gain = information_gain  # if better than current best, set to best
+            selected_attribute = i
+    return selected_attribute  # returns an integer
 
 def classify(inputTree, test_data):
+    '''
+
+    '''
+
     labels = list(test_data.keys())
     test_values = list(test_data.values())
     root_node = list(inputTree.keys())[0]
@@ -136,7 +143,6 @@ def classify(inputTree, test_data):
     labels_index = labels.index(root_node)
     key = test_values[labels_index]
     value = branches[key]
-
     if isinstance(value, dict):
         classLabel = classify(value, test_data)
     else:
@@ -146,7 +152,7 @@ def classify(inputTree, test_data):
 if __name__ == "__main__":
 
     # FROM CSV
-    myDat, labels = createDataSetCsv(training_data1) #for given training data
+    myDat, labels = createDataTableCsv(training_data1) #for given training data
     mytree1 = createTree(myDat, labels)
     #print(mytree1)
 
@@ -155,8 +161,7 @@ if __name__ == "__main__":
     print(answer)
 
     # FOR HW
-
-    myDat1, labels1 = createDataSet(training_data2)  # for given training data
+    myDat1, labels1 = createDataTable(training_data2)  # for given training data
     mytree2 = createTree(myDat1, labels1)
     #print(mytree2)
 
@@ -168,4 +173,4 @@ if __name__ == "__main__":
     print(answer1, answer2)
 
     #print(createDataTable(training_data2))
-    #print(createDataSetCsv(training_data1))
+    #print(createDataTableCsv(training_data1))
